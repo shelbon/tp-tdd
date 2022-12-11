60bbb331c432d4f0901e0629f645c07490a1b86c
shelbon <mouclepatrick@protonmail.com>
2022-12-11 18:46:49 +0100

    unit test save   driving license - refactor

--- src/main/java/fr/esgi/cleancode/validator/SocialSecurityNumberValidator.java
+++ src/main/java/fr/esgi/cleancode/validator/SocialSecurityNumberValidator.java
@@ -1,46 +1,22 @@
 package fr.esgi.cleancode.validator;
 
 import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
-import org.graalvm.collections.Pair;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public final class SocialSecurityNumberValidator {
 
-    private static final String INVALID_SIZE = "No social security numbers provided";
-    private static final String INVALID_LENGTH = "invalid length social security number should have 15 characters";
-    public static final String INVALID_CHARACTERS = "Should only contains numbers";
+    private static final String NO_SOCIAL_SECURITY_NUMBERS_PROVIDED = "No social security numbers provided";
+    private static final String SHOULD_HAVE_15_CHARACTERS = "Invalid length social security number should have 15 characters";
+    public static final String SHOULD_ONLY_CONTAINS_NUMBERS = "Should only contains numbers";
 
     public void validate(String socialSecurityNumber) {
-        boolean isNull = socialSecurityNumber == null;
-        boolean isLessThan15Characters = (!isNull && socialSecurityNumber.length() < 15);
-        boolean doesNotContainOnlyNumbers = (!isNull && !socialSecurityNumber.matches("^[0-9]+$"));
-        HashMap<String, Boolean> errorsCheck = new HashMap<>();
-        errorsCheck.put(INVALID_SIZE, isNull);
-        errorsCheck.put(INVALID_LENGTH, isLessThan15Characters);
-        errorsCheck.put(INVALID_CHARACTERS, doesNotContainOnlyNumbers);
-        String errorOutput = buildErrorOutput(errorsCheck);
-        if (errorOutput.length() > 0) {
-            throw new InvalidDriverSocialSecurityNumberException(errorOutput);
+        if (socialSecurityNumber == null) {
+            throw new InvalidDriverSocialSecurityNumberException(NO_SOCIAL_SECURITY_NUMBERS_PROVIDED);
         }
-
+        if (socialSecurityNumber.length() < 15) {
+            throw new InvalidDriverSocialSecurityNumberException(SHOULD_HAVE_15_CHARACTERS);
+        }
+        if (!socialSecurityNumber.matches("^\\d+$")) {
+            throw new InvalidDriverSocialSecurityNumberException(SHOULD_ONLY_CONTAINS_NUMBERS);
+        }
     }
-
-    private String buildErrorOutput(HashMap<String, Boolean> errorsCheck) {
-        StringBuilder errorOutput = new StringBuilder();
-        for (var errorCheck : errorsCheck.entrySet()) {
-            if (Boolean.TRUE.equals(errorCheck.getValue())) {
-                errorOutput.append("- ")
-                        .append(errorCheck.getKey())
-                        .append("\n");
-            }
-        }
-        if(errorOutput.length() > 0){
-            errorOutput.insert(0, "Errors happened when validating social security nummber:\n");
-        }
-        return errorOutput.toString();
-    }
-
-
 }
--- src/test/java/fr/esgi/cleancode/validator/SocialSecurityNumberValidatorTest.java
+++ src/test/java/fr/esgi/cleancode/validator/SocialSecurityNumberValidatorTest.java
@@ -4,6 +4,7 @@ import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -11,25 +12,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOf
 class SocialSecurityNumberValidatorTest {
     @InjectMocks
     private SocialSecurityNumberValidator validator;
+    @Mock
+    private SocialSecurityNumberValidatorRule validationRule;
 
     @Test
-    void should_throw_invalid_driver_social_security_number_exception_when_social_security_empty() {
-        String socialSecurityNumber = "";
+    void should_throw_invalid_driver_social_security_number_exception_when_social_security_null() {
+        String socialSecurityNumber = null;
         assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class)
-                .isThrownBy(() -> validator.validate(socialSecurityNumber));
+                .isThrownBy(() -> validator.validate(socialSecurityNumber))
+                .withMessage("No social security numbers provided");
     }
 
     @Test
     void should_throw_invalid_driver_social_security_number_exception_when_social_security_less_than_15() {
         String socialSecurityNumber = "12478593123547";
+
+
         assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class)
-                .isThrownBy(() -> validator.validate(socialSecurityNumber));
+                .isThrownBy(() -> validator.validate(socialSecurityNumber))
+                .withMessage("Invalid length social security number should have 15 characters");
     }
 
     @Test
     void should_throw_invalid_driver_social_security_number_exception_when_social_security_contains_not_contains_only_numbers() {
         String socialSecurityNumber = "12478593123547a";
         assertThatExceptionOfType(InvalidDriverSocialSecurityNumberException.class)
-                .isThrownBy(() -> validator.validate(socialSecurityNumber));
+                .isThrownBy(() -> validator.validate(socialSecurityNumber))
+                .withMessage("Should only contains numbers");
     }
 }
